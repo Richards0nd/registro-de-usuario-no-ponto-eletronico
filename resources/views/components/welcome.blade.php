a2db9fdf046a6e9e7d955b393e5c24619a36077f
Richards0nd <richardson.douglas.31@gmail.com>
2023-11-16 03:50:21 -0300

    chore: :lipstick: Atualizada a interface principal e o dashboard

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -1,14 +1,9 @@
 <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
-
     <h1 class="mt-8 text-2xl font-medium text-gray-900">
         Bem vindo ao registro de usuários no ponto eletrônico
     </h1>
-
     <p class="mt-6 text-gray-500 leading-relaxed">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio minus, cum tenetur sunt inventore natus eius
-        enim iusto, doloremque nulla ut voluptatem eos eligendi esse eum, ex consectetur id ad!
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi quae nesciunt numquam mollitia nam. Enim cum
-        velit illum non, quo atque commodi eos voluptatem eaque blanditiis aperiam voluptates ipsa itaque.
+        Sistema para registro de funcionários que irão utilizar o ponto eletrônico!
     </p>
 </div>
 
@@ -32,7 +27,7 @@
         </p>
 
         <p class="mt-4 text-sm">
-            <a href="https://laravel.com/docs" class="inline-flex items-center font-semibold text-indigo-700">
+            <a href="/docs" class="inline-flex items-center font-semibold text-indigo-700">
                 Visite a documentação da aplicação
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ml-1 w-5 h-5 fill-indigo-500">
                     <path fill-rule="evenodd"
@@ -51,7 +46,7 @@
                     d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
             </svg>
             <h2 class="ml-3 text-xl font-semibold text-gray-900">
-                <a href="https://laracasts.com">Demonstração em Vídeo</a>
+                <a href="/video">Demonstração em Vídeo</a>
             </h2>
         </div>
 
@@ -62,7 +57,7 @@
         </p>
 
         <p class="mt-4 text-sm">
-            <a href="https://laracasts.com" class="inline-flex items-center font-semibold text-indigo-700">
+            <a href="/video" class="inline-flex items-center font-semibold text-indigo-700">
                 Ver vídeo de apresentação
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ml-1 w-5 h-5 fill-indigo-500">
                     <path fill-rule="evenodd"
@@ -73,41 +68,4 @@
         </p>
     </div>
 
-    {{-- <div>
-        <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                class="w-6 h-6 stroke-gray-400">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-            </svg>
-            <h2 class="ml-3 text-xl font-semibold text-gray-900">
-                <a href="https://tailwindcss.com/">Tailwind CSS</a>
-            </h2>
-        </div>
-
-        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-            Laravel Jetstream is built with Tailwind, an amazing utility first CSS framework that doesn't get in your
-            way. You'll be amazed how easily you can build and maintain fresh, modern designs with this wonderful
-            framework at your fingertips.
-        </p>
-    </div>
-
-    <div>
-        <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                class="w-6 h-6 stroke-gray-400">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
-            </svg>
-            <h2 class="ml-3 text-xl font-semibold text-gray-900">
-                Authentication
-            </h2>
-        </div>
-
-        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-            Authentication and registration views are included with Laravel Jetstream, as well as support for user email
-            verification and resetting forgotten passwords. So, you're free to get started with what matters most:
-            building your application.
-        </p>
-    </div> --}}
 </div>
